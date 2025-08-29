72888e8a969198990a12bd5956a4ee94775d2ac7
Luna <ciubix8514@gmail.com>
2025-08-29 20:55:04 +0400

    Optimaize the code A LOT

--- metrics_collection.lua
+++ metrics_collection.lua
@@ -1,11 +1,11 @@
 local item_list = require("item_list")
 local component = require("component")
 
-local function extract_eu(str)
+local function extract_eu(str, ind)
   local num = 1
   for i in str:gmatch("[^%s]+") do
-    if num == 4 then
-      return i:gsub(",", "")
+    if num == ind then
+      return i:gsub(",", ""):gsub("§c", "")
     end
     num = num + 1
   end
@@ -16,13 +16,16 @@ local function get_metrics()
   local index = 1
   local metrics = {}
 
+  local me_items = component.me_interface.getItemsInNetwork()
+
   for _, i in pairs(i_list) do
     local size = 0
-
-    local item = component.me_interface.getItemsInNetwork({ label = i })
-
-    if item[1] ~= nil then
-      size = item[1].size
+    for _, me_item in pairs(me_items) do
+      if me_item.label == i then
+        -- This is the correct item
+        size = me_item.size
+        break
+      end
     end
 
     metrics[index] = {
@@ -40,11 +43,12 @@ local function get_metrics()
   end
 
   local f_list = item_list.get_fluids()
+  local me_fluids = component.me_interface.getFluidsInNetwork()
 
   for _, i in pairs(f_list) do
     local count = 0
 
-    for _, f in pairs(component.me_interface.getFluidsInNetwork()) do
+    for _, f in pairs(me_fluids) do
       if f.label == i then
         count = f.amount
       end
@@ -66,13 +70,14 @@ local function get_metrics()
 
   local res, gt = pcall(component.getPrimary, "gt_machine")
   if res == true then
-    local eu           = gt.getEUStored()
     local data         = gt.getSensorInformation()
 
+    local eu           = extract_eu(data[2], 3)
     -- Avg EU IN: 102,007,470 (last 5 seconds)
-    local eui          = extract_eu(data[10])
+    local eui          = extract_eu(data[10], 4)
     -- Avg EU OUT: 102,007,470 (last 5 seconds)
-    local euo          = extract_eu(data[11])
+    local euo          = extract_eu(data[11], 4)
+    local w_eu         = extract_eu(data[23], 4)
 
     metrics[index]     = {
       type = "energy",
@@ -88,6 +93,11 @@ local function get_metrics()
       type = "energy",
       name = "eu_output",
       count = euo
+    }
+    metrics[index + 3] = {
+      type = "energy",
+      name = "wireless_eu",
+      count = w_eu,
     }
   end
 
